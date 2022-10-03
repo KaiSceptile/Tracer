d444694b0a8bd88ec21eb5f930411451c9eb22de
KaiSceptile <90106146+KaiSceptile@users.noreply.github.com>
2022-10-04 00:55:57 +0300

    adding program with method with 3 internal methods

--- TracerApp/Program.cs
+++ TracerApp/Program.cs
@@ -1,2 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TracerLibrary;
+using TracerLibrary.Serializing;
+
+namespace TracerApp
+{
+    class Program
+    {
+        public Tracer Tracer { get; set; }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Perform();
+        }
+
+        public void Perform()
+        {
+            this.Tracer = new Tracer();
+
+            TestMethod_1();
+            
+
+            SaveToXml();
+            SaveToJson();
+
+            Console.ReadLine();
+        }
+
+        public void TestMethod_1()
+        {
+            this.Tracer.StartTrace();
+            TestMethod_1_1();
+            TestMethod_1_2();
+            TestMethod_1_3();
+            this.Tracer.StopTrace();
+        }
+
+        public void TestMethod_1_1()
+        {
+            this.Tracer.StartTrace();
+            Thread.Sleep(100);
+            this.Tracer.StopTrace();
+        }
+
+        public void TestMethod_1_2()
+        {
+            this.Tracer.StartTrace();
+            Thread.Sleep(100);
+            this.Tracer.StopTrace();
+        }
+
+        public void TestMethod_1_3()
+        {
+            this.Tracer.StartTrace();
+            Thread.Sleep(100);
+            this.Tracer.StopTrace();
+        }
+
+        
+
+        public void SaveToXml()
+        {
+            string pathToSave = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\TestResults\\XmlTraсeResult.xml");
+
+            FileStream fileStream = new FileStream(pathToSave, FileMode.Create);
+            StreamWriter streamWriter = new StreamWriter(fileStream);
+            TraceResult tracerResult = this.Tracer.GetTraceResult();
+
+            xmlSerializer XmlSerializer = new xmlSerializer();
+            XmlSerializer.SaveTraceResult(streamWriter, tracerResult);
+            XmlSerializer.SaveTraceResult(Console.Out, tracerResult);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public void SaveToJson()
+        {
+            string pathToSave = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\TestResults\\JsonTraсeResult.json");
+
+            FileStream fileStream = new FileStream(pathToSave, FileMode.Create);
+            StreamWriter streamWriter = new StreamWriter(fileStream);
+            TraceResult tracerResult = this.Tracer.GetTraceResult();
+
+            JSONSerializer jsonSerializer = new JSONSerializer();
+            jsonSerializer.SaveTraceResult(streamWriter, tracerResult);
+            jsonSerializer.SaveTraceResult(Console.Out, tracerResult);
+        }
+    }
+}
